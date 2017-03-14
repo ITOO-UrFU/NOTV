3d6241dcfb11f46fdbf21ff82071c6b6b43137be
MasterGowen <mastergowen@gmail.com>
2017-03-14 19:32:45 +0500

    Generating Tokens signal receiver

--- notv/settings.py
+++ notv/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reversion',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'admin_reorder',
     'core',
