fc3da2152f8ee219046bd0996256b6995baadd3c
MasterGowen <mastergowen@gmail.com>
2017-03-16 14:38:12 +0500

    get_queryset

--- apps/api/views.py
+++ apps/api/views.py
@@ -187,4 +187,13 @@ class PersonUpdate(generics.UpdateAPIView):
         else:
             return None
 
+    def get_queryset(self):
+        """
+        Adding this method since it is sometimes called when using
+        django-rest-swagger
+        https://github.com/Tivix/django-rest-auth/issues/275
+        """
+
+        return Person.objects.get(user=self.request.user)
+
 
