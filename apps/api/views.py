065321da68ce613db395d4bcc5ce678bba16d4ff
MasterGowen <mastergowen@gmail.com>
2017-03-16 14:10:33 +0500

    PersonUpdate

--- apps/api/views.py
+++ apps/api/views.py
@@ -123,18 +123,14 @@ class PersonDetailsView(generics.RetrieveUpdateAPIView):
 
     def get_object(self, request):
         jwt_token = self.request.META.get('HTTP_AUTHORIZATION', None)
-        print(jwt_token, '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         if jwt_token:
             token_data = jwt.decode(jwt_token, settings.SECRET_KEY)
             current_user = User.objects.get(pk=token_data['user_id'])
-            print(current_user, '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
             try:
                 person = Person.objects.get(user=current_user)
-                print(person, '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
             except:
                 person = Person(user=current_user, first_name=_("Name"), last_name=_("Last name"))
-                print(person, 'CREATED    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
                 person.save()
 
             return person
@@ -150,5 +146,45 @@ class PersonDetailsView(generics.RetrieveUpdateAPIView):
         """
 
         return Person.objects.get(user=self.request.user)
+
+
+class PersonUpdate(generics.UpdateAPIView):
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object(request)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    def get_object(self, request):
+        """
+        Returns the object the view is displaying.
+
+        You may want to override this if you need to provide non-standard
+        queryset lookups.  Eg if objects are referenced using multiple
+        keyword arguments in the url conf.
+        """
+        jwt_token = self.request.META.get('HTTP_AUTHORIZATION', None)
+        if jwt_token:
+            token_data = jwt.decode(jwt_token, settings.SECRET_KEY)
+            current_user = User.objects.get(pk=token_data['user_id'])
+
+            try:
+                person = Person.objects.get(user=current_user)
+            except:
+                person = Person(user=current_user, first_name=_("Name"), last_name=_("Last name"))
+                person.save()
+
+            return person
+
+        else:
+            return None
 
 
