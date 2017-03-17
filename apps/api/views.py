74dfca1208badf4fa5bd555a9465052eb9f72077
MasterGowen <mastergowen@gmail.com>
2017-03-17 18:35:49 +0500

    try registration #11 refactoring, exceptions

--- apps/api/views.py
+++ apps/api/views.py
@@ -222,23 +222,27 @@ class PersonUpdate(generics.UpdateAPIView):
 @api_view(('POST',))
 @permission_classes((permissions.AllowAny,))
 def register_on_event(request):
-    #
-    # try:
 
-    jwt_token = request.META.get('HTTP_AUTHORIZATION', None)
-    # if jwt_token:
-    token_data = jwt.decode(jwt_token, settings.SECRET_KEY)
-    current_user = User.objects.get(pk=token_data['user_id'])
-    person = Person.objects.get(user=current_user)
-    event = Event.objects.get(id=request.data.get('event_id'))
-    type = RegistrationType.objects.filter(title="Участник").first()
-    eur = EventUserRegistration(person=person, event=event, status="r", type=type)
-    eur.save()
+     try:
 
-    return Response({"success": True})
-    # else:
-    #         return Response({"success": False})
+        jwt_token = request.META.get('HTTP_AUTHORIZATION', None)
+        if jwt_token:
+            token_data = jwt.decode(jwt_token, settings.SECRET_KEY)
+            current_user = User.objects.get(pk=token_data['user_id'])
+            person = Person.objects.get(user=current_user)
+            event = Event.objects.get(id=request.data.get('event_id'))
+            type = RegistrationType.objects.filter(title="Участник").first()
 
-    # except:
-    #
-    #     return Response({"success": False})
+            try:
+                eur = EventUserRegistration(person=person, event=event, status="r", type=type)
+                if eur:
+                    return Response({"success": False})
+            except:
+                eur = EventUserRegistration.objects.filter(person=person, event=event)
+                eur.save()
+
+            return Response({"success": True})
+        else:
+                 return Response({"success": False})
+     except:
+         return Response({"success": False})
