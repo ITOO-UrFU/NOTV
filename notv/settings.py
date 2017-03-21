9c376aa15ca184e8fad80c8d1585fa061ab4f384
MasterGowen <mastergowen@gmail.com>
2017-03-21 11:02:04 +0500

    set big JWT_EXPIRATION_DELTA

--- notv/settings.py
+++ notv/settings.py
@@ -251,12 +251,12 @@ JWT_AUTH = {
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
     'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=30000),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=14),
     'JWT_AUDIENCE': None,
     'JWT_ISSUER': None,
 
     'JWT_ALLOW_REFRESH': False,
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=14),
 
     'JWT_AUTH_HEADER_PREFIX': 'JWT',
 }
