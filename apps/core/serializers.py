4b2423eeb7fb13167021a53d5e7b5676a0fd8a6c
MasterGowen <mastergowen@gmail.com>
2017-03-27 16:55:01 +0500

    add get_speakers

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -126,7 +126,7 @@ class EventSerializer_noperson(serializers.ModelSerializer):
 
     class Meta:
         model = Event
-        fields = ("id", "title", "description", "get_block_slug", "get_type_display", "get_event_slug", "startdate", "enddate")
+        fields = ("id", "title", "description", "get_block_slug", "get_speakers", "get_type_display", "get_event_slug", "startdate", "enddate")
 
 
 class EventUserRegistrationSerializer_noperson(serializers.ModelSerializer):
