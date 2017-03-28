ab714f7eb6d920583d8e1b499a4b6786f3601ac5
MasterGowen <mastergowen@gmail.com>
2017-03-28 18:14:33 +0500

    impl parse() #13.1

--- apps/api/views.py
+++ apps/api/views.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from rest_framework import viewsets, views
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.parsers import FileUploadParser
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework import permissions
