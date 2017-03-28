ce13231568a84b278cd341733ff67df6cc6cdc3f
MasterGowen <mastergowen@gmail.com>
2017-03-28 18:51:31 +0500

    USE_TZ = False #1

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 class DateTimeFieldWihTZ(serializers.DateTimeField):
 
     def to_representation(self, value):
-        value = timezone.localtime(value)
+        # value = timezone.localtime(value)
         return super(DateTimeFieldWihTZ, self).to_representation(value)
 
 
