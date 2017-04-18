c651ed17a3c0126fe7a7647d02a106bab23c12e4
MasterGowen <mastergowen@gmail.com>
2017-04-18 14:30:53 +0500

    members.html#5 cache

--- apps/stats/views.py
+++ apps/stats/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 from django.contrib.admin.views.decorators import staff_member_required
 
 from core.models import *
 
 
 @staff_member_required
+@cache_page(60 * 5)
 def events_members(request):
     context = {}
     events = Event.objects.all()
