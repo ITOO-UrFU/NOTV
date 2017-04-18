ed8842c2207b3d0e3e50199906843e6c7c91cfc5
MasterGowen <mastergowen@gmail.com>
2017-04-18 13:20:45 +0500

    members.html#2

--- apps/stats/views.py
+++ apps/stats/views.py
@@ -7,10 +7,11 @@ from core.models import *
 @staff_member_required
 def events_members(request):
     context = {}
+    context["event_members"] = {}
     events = Event.objects.all()
     context["events"] = events
     for event in events:
         members = [x.person for x in EventUserRegistration.objects.filter(event=event)]
-        context[event.title] = members
+        context["event_members"][event.title] = members
 
     return render(request, 'members.html', context)
