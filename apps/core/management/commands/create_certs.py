7b8c8502934fdf3e3bb0682dadbd7f642aaf0b0f
MasterGowen <mastergowen@gmail.com>
2017-05-04 10:54:46 +0500

    diplomas#9

--- apps/core/management/commands/create_certs.py
+++ apps/core/management/commands/create_certs.py
@@ -21,7 +21,8 @@ class Command(BaseCommand):
 
             packet = io.StringIO()
             can = canvas.Canvas(packet, pagesize=letter)
-            can.drawString(10, 100, " ".join[person.first_name, person.last_name])
+            name = " ".join[person.first_name, person.last_name]
+            can.drawString(10, 100, name)
             can.save()
 
             packet.seek(0)
