facbc70311fbed0f9ff6b9028ffe5f2a2e44cb3f
MasterGowen <mastergowen@gmail.com>
2017-05-04 10:53:24 +0500

    diplomas#7

--- apps/core/management/commands/create_certs.py
+++ apps/core/management/commands/create_certs.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 
 from PyPDF2 import PdfFileWriter, PdfFileReader
-import StringIO
+import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
