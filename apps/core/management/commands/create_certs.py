5a2e3e0385eae52c6ab11c9178b0405d4841ceff
MasterGowen <mastergowen@gmail.com>
2017-05-04 11:37:44 +0500

    diplomas#25

--- apps/core/management/commands/create_certs.py
+++ apps/core/management/commands/create_certs.py
@@ -6,7 +6,12 @@ from django.conf import settings
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import io
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfbase.pdfmetrics import stringWidth
+from reportlab.rl_config import defaultPageSize
+
+PAGE_WIDTH  = defaultPageSize[0]
+PAGE_HEIGHT = defaultPageSize[1]
 
 from core.models import Person
 
@@ -21,8 +26,15 @@ class Command(BaseCommand):
         for person in Person.objects.all():
 
             packet = io.BytesIO()
-            can = canvas.Canvas(packet, pagesize=letter)
+            can = canvas.Canvas(packet, pagesize=A4)
             name = "{} {}".format(str(person.first_name), str(person.last_name))
+
+            text_width = stringWidth(name)
+            y = 1050
+
+            pdf_text_object = canvas.beginText((PAGE_WIDTH - text_width) / 2.0, y)
+            pdf_text_object.textOut(name)
+
             can.drawString(10, 100, name)
             can.save()
 
