13c4956d4139d91e2e0d92b2fcc802bee41e519d
MasterGowen <mastergowen@gmail.com>
2017-05-04 11:17:47 +0500

    diplomas#21

--- apps/core/management/commands/create_certs.py
+++ apps/core/management/commands/create_certs.py
@@ -28,7 +28,7 @@ class Command(BaseCommand):
 
             packet.seek(0)
             new_pdf = PdfFileReader(packet)
-            existing_pdf = PdfFileReader(codecs.open(os.path.join(settings.MEDIA_ROOT, "certificate.pdf"), "rb", encoding='utf-8'))
+            existing_pdf = PdfFileReader(codecs.open(os.path.join(settings.MEDIA_ROOT, "certificate.pdf"), "r", encoding='utf-8'))
             output = PdfFileWriter()
 
 
@@ -36,6 +36,6 @@ class Command(BaseCommand):
             page.mergePage(new_pdf.getPage(0))
             output.addPage(page)
 
-            outputStream = codecs.open(os.path.join(settings.MEDIA_ROOT, "diplomas", str(person.id) + "-" + "diploma.pdf"), "wb", encoding='utf-8')
+            outputStream = codecs.open(os.path.join(settings.MEDIA_ROOT, "diplomas", str(person.id) + "-" + "diploma.pdf"), "w", encoding='utf-8')
             output.write(outputStream)
             outputStream.close()
