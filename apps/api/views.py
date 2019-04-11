eb794e46cac5c0f4802326c00ba99d053091b605
MasterGowen <mastergowen@gmail.com>
2019-04-11 18:53:27 +0500

    speakers api without photo

--- apps/api/views.py
+++ apps/api/views.py
@@ -80,8 +80,8 @@ class Speakers(viewsets.ModelViewSet):  # CacheResponseMixin
         speakers = Person.objects.filter(
             Q(id__in=EventUserRegistration.objects.filter(type__in=types).values('person_id')) |
             Q(user=None)).filter(
-            ~Q(position="студент") &
-            ~Q(photo="")
+            ~Q(position="студент") #&
+            #~Q(photo="")
         )
 
         return speakers.order_by("-karma")
